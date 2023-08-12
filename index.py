d8e79ca3ac4209ffd4309effcb3786571cc7775e
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-08-12 19:29:31 +0300

    shows how to change color for the turtle

--- index.py
+++ index.py
@@ -3,3 +3,9 @@
 from turtle import Turtle
 timmy_the_turtle = Turtle()
 
+#shows how to change color for the turtle
+timmy_the_turtle.color('red')
+
+#shows how to exit screen for turtle on click
+screen = Screen()
+screen.exitonclick()
