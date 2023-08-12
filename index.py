e9c8fdfef79983b40f9c5800d1227bccc3c759e8
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-08-12 19:36:33 +0300

    shows how to move the turtle forward 100 steps

--- index.py
+++ index.py
@@ -9,7 +9,8 @@ timmy_the_turtle.color('red')
 #shows how to change shape for turtle
 timmy_the_turtle.shape('turtle')
 
-
+#shows how to move the turtle 100 steps forward
+timmy_the_turtle.forward(100)
 
 #shows how to exit screen for turtle on click
 screen = Screen()
