862b2123991de97c0e6de3bf5f7a7e4e045df262
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-08-12 21:01:05 +0300

    shows how to create a random walk using turtle graphics with selection of colors from the rgb tuple

--- randomcolorsselection.py
+++ randomcolorsselection.py
@@ -1,20 +1,24 @@
 #shows how to use turtle graphics to create a random walk
+#shows how to create random colors using tuples
 import turtle as t
 import random
 
 tim = t.Turtle()
+t.colormode(255)
 
-colors = ['CornflowerBlue','DarkOrchid','IndianRed','DeepSkyBlue',
-          'LightSeaGreen','wheat','SlateGray','SeaGreen',
-          'medium aquamarine']
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r,g,b)
+    return random_color
+
 directions = [0, 90, 170, 270]
 tim.pensize(15)
 tim.speed("fastest")
  
 for _ in range(200):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
-
-#shows how to create random colors using tuples
 
